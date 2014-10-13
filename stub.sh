371e73e01b7f5579f4415ce32d980788c5eb72b7
Dominick Grift <dac.override@gmail.com>
2014-10-13 17:17:06 +0200

    The "Stub-Split" SELinux security policy model
    After "Stub", this is the most basic SELinux security policy model
    
    A minimal set of additional type identifiers are implemented for the various objects provided by the Linux OM, and accompanying context specifications are implemented where applicable
    
     - Linux objects are grouped and associated with base classification attributes
     - base type security identifiers for network related objects like ports, nodes, network interfaces and packets are declared in the net.cil module
     - base type security identifiers for filesystem objects are declared in the fs.cil policy module
     - a base type security identifier for common files is implemented in the file.cil module
     - a type security identifier for the null device file is implemented in the dev.cil module
     - a type security identifier for the security filesystem is implemented in the sec.cil module
     - a type security identifier for the unlabeled isid is implemented in the existing entity.cil module
     - a type security identifier for the file isid is implemented in the object.cil module
     - mls, polcap, config, av and isid related statements and declarations were split out to their own respective modules
     - a new module called sys.cil, and security identifiers were created for valid processes
     - existing identifiers that became redundant in entity.cil were removed
     - contexts configuration files were modified to reflect changes
     - basic unconfined access to the various objects and identifiers was implemented
    
    The sole (valid) process type identifier "s.sys.t" has unrestricted access to most Linux objects and types
    
    Some temporary workarounds were implemented to make this basic policy model just work

--- stub.sh
+++ stub.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-/usr/bin/secilc -v -D source/entity.cil -M false -o policy/policy.29 -f contexts/files/file_contexts
+/usr/bin/secilc -v -D \
+    source/av.cil \
+    source/base/dev.cil \
+    source/base/file.cil \
+    source/base/fs.cil \
+    source/base/net.cil \
+    source/base/sec.cil \
+    source/base/sys.cil \
+    source/config.cil \
+    source/entity.cil \
+    source/isid.cil \
+    source/mls.cil \
+    source/object.cil \
+    source/polcap.cil \
+    source/subject.cil \
+    -M false -o policy/policy.29 -f contexts/files/file_contexts
